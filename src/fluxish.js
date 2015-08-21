82d7704020e9ea5acadb7f3f522d1a2efe3c25b2
Matthew Hall <mhall@bouncingpixel.com>
2015-08-21 17:08:38 -0500

    Refactoring to clean up areas and bring sloc down lower

--- src/fluxish.js
+++ src/fluxish.js
@@ -1,16 +1,8 @@
 class Fluxish {
   constructor() {
     this._storeStates = {};
-    this._stores = {};
-    this._actions = {};
-    this._writableContext = {
-      stores: {},
-      actions: {}
-    };
-    this._readOnlyContext = {
-      stores: {},
-      actions: {}
-    };
+    this._writableContext = {stores: {}, actions: {}};
+    this._readOnlyContext = {stores: {}, actions: {}};
     this._listeners = {};
   }
 
@@ -18,86 +10,77 @@ class Fluxish {
     return this._readOnlyContext;
   }
 
-  loadStores(stores, replaceIfExists = false) {
+  loadStores(stores) {
     for (const storeName in stores) {
-      const notExisting = !this._stores.hasOwnProperty(storeName);
-      if (notExisting || replaceIfExists) {
-        const store = stores[storeName];
+      const store = stores[storeName];
 
-        let storeWriteContext = {};
-        let storeReadContext = {};
+      let storeWriteContext = {};
+      let storeReadContext = {};
 
-        for (const funtionName in store.mutators) {
-          storeWriteContext[funtionName] = this._createMutatorFuncWrapper(store.mutators[funtionName], storeName);
-        }
+      for (const funtionName in store.mutators) {
+        storeWriteContext[funtionName] = _wrapMutator.call(this, store.mutators[funtionName], storeName, store.mutators);
+      }
 
-        for (const funtionName in store.getters) {
-          const wrappedFunction = this._createGetterFuncWrapper(store.getters[funtionName], storeName);
-          storeReadContext[funtionName] = wrappedFunction;
-          storeWriteContext[funtionName] = wrappedFunction;
-        }
+      for (const funtionName in store.getters) {
+        const wrappedFunction = _wrapGetter.call(this, store.getters[funtionName], storeName, store.getters);
+        storeWriteContext[funtionName] = storeReadContext[funtionName] = wrappedFunction;
+      }
 
-        this._writableContext.stores[storeName] = storeWriteContext;
-        this._readOnlyContext.stores[storeName] = storeReadContext;
-        this._stores[storeName] = store;
+      this._writableContext.stores[storeName] = storeWriteContext;
+      this._readOnlyContext.stores[storeName] = storeReadContext;
 
-        if (notExisting && store.initialState) {
+      if (!this._storeStates.hasOwnProperty(storeName)) {
+        this._listeners[storeName] = [];
+
+        if (store.initialState) {
           this._storeStates[storeName] = store.initialState();
         }
       }
     }
   }
 
-  loadActions(actions, replaceIfExists = false) {
+  loadActions(actions) {
     for (const actionName in actions) {
-      if (!this._actions.hasOwnProperty(actionName) || replaceIfExists) {
-        const action = actions[actionName];
+      const action = actions[actionName];
 
-        const wrappedFunction = this._createActionFuncWrapper(action);
+      const wrappedFunction = _wrapAction.call(this, action);
 
-        this._writableContext.actions[actionName] = wrappedFunction;
-        this._readOnlyContext.actions[actionName] = wrappedFunction;
-        this._actions[actionName] = action;
-      }
+      this._writableContext.actions[actionName] = this._readOnlyContext.actions[actionName] = wrappedFunction;
     }
   }
 
   listen(storeName, listener) {
-    if (!this._listeners.hasOwnProperty(storeName)) {
-      this._listeners[storeName] = [listener];
-    } else {
-      this._listeners[storeName].push(listener);
+    const storeListeners = this._listeners[storeName];
+    if (storeListeners.indexOf(listener) === -1) {
+      storeListeners.push(listener);
     }
 
     return () => {
-      const index = this._listeners[storeName].indexOf(listener);
-      this._listeners[storeName].splice(index, 1);
-    };
-  }
-
-  _createMutatorFuncWrapper(fn, storeName) {
-    return (...params) => {
-      let newData = fn.apply(this._stores[storeName].mutators, [this._readOnlyContext, this._storeStates[storeName], ...params]);
-      if (newData !== undefined) {
-        this._storeStates[storeName] = newData;
-        if (this._listeners[storeName]) {
-          this._listeners[storeName].forEach(listener => listener(this._readOnlyContext));
-        }
-      }
-    };
-  }
-
-  _createGetterFuncWrapper(fn, storeName) {
-    return (...params) => {
-      return fn.apply(this._stores[storeName].getters, [this._storeStates[storeName], ...params]);
-    };
-  }
-
-  _createActionFuncWrapper(fn) {
-    return (...params) => {
-      return fn(this._writableContext, ...params);
+      storeListeners.splice(storeListeners.indexOf(listener), 1);
     };
   }
 }
 
 export default Fluxish;
+
+function _wrapMutator(fn, storeName, ctx) {
+  return (...params) => {
+    const newData = fn.apply(ctx, [this._readOnlyContext, this._storeStates[storeName], ...params]);
+    if (newData !== undefined) {
+      this._storeStates[storeName] = newData;
+      this._listeners[storeName].forEach(listener => listener(this._readOnlyContext));
+    }
+  };
+}
+
+function _wrapGetter(fn, storeName, ctx) {
+  return (...params) => {
+    return fn.apply(ctx, [this._storeStates[storeName], ...params]);
+  };
+}
+
+function _wrapAction(fn) {
+  return (...params) => {
+    return fn(this._writableContext, ...params);
+  };
+}
